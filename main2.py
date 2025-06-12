7e92f466f76db6129590461e7ca000557a6019a3
@Mohammad_Dev <ouyoussefmohamedreda2@gmail.com>
2025-06-12 12:58:59 +0100

    Update main2.py

--- main2.py
+++ main2.py
@@ -1,30 +1,30 @@
 import webbrowser
 
 while True:
-  quser = input("open type (system|Programming|design|drawing|internet|Freelance|games|chine) : ").lower()
+  quser = input("open type (system|programming|design|drawing|internet|freelance|games|chine) : ").lower()
 #  if quser == "system":
-  if quser == "Programming":
-    quser1 = input("open (Visual Studio Code (vsc)|GitHub|Git|Command Prompt (cmd.exe)|app development|Game development) : ").lower()
+  if quser == "programming":
+    quser1 = input("open (Visual Studio Code (vsc)|github|git|command prompt (cmd.exe)|app development|game development) : ").lower()
 #    if quser1 == "Visual Studio Code (vsc)" or quser1 == "Visual Studio Code" or quser1 == "vsc":
-    if quser1 == "GitHub":
+    if quser1 == "github":
       webbrowser.open("https://github.com/mohamedredaou") 
-      print("Opening GitHub...")
-#    elif quser1 == "Git":
-#    elif quser1 == "Command Prompt (cmd.exe)" or quser1 == "Command Prompt" or quser1 == "cmd" or quser1 == "cmd.exe":
+      print("Opening github...")
+#    elif quser1 == "git":
+#    elif quser1 == "command prompt (cmd.exe)" or quser1 == "command prompt" or quser1 == "cmd" or quser1 == "cmd.exe":
 #    elif quser1 == "app development":
-#      quser2 = input("open (Flutter|android developer studio (ads)) : ").lower()
+#      quser2 = input("open (flutter|android developer studio (ads)) : ").lower()
 #      if quser2 == "android developer studio (ads)" or quser1 == "android developer studio" or quser1 == "ads":
-#      elif quser2 == "Flutter":
+#      elif quser2 == "flutter":
 #      else:
 #        print("This app & web-site does not exist")
-#    elif quser1 == "Game development":
-#      quser3 = input("open (godot|Unreal Engine 5) : ").lower()
+#    elif quser1 == "game development":
+#      quser3 = input("open (godot|unreal engine 5) : ").lower()
 #      if quser3 == "godot":
-#      elif quser3 == "Unreal Engine 5":
+#      elif quser3 == "unreal engine 5":
 #      else:
 #        print("This app & web-site does not exist")
-#    else:
-#      print("This app & web-site does not exist")
+    else:
+      print("This app & web-site does not exist")
   elif quser == "design":
     quser5 = input("open (unblast|unsplash|font|freepik|dribbble|behance|sketchfab|free3d|edit video & image|design ui, ux|graphic design|3D design|Social media design) : ").lower()
     if quser5 == "unblast":
@@ -85,7 +85,7 @@ while True:
 #      elif quser9 == "SketchUp":
 #      else:
 #        print("This app & web-site does not exist")
-    elif quser5 == "Social media design":
+    elif quser5 == "social media design":
       quser10 = input("open (photopea) : ").lower()
       if quser10 == "photopea":
         webbrowser.open("https://photopea.com/") 
@@ -95,12 +95,12 @@ while True:
     else:
       print("This app & web-site does not exist")
   elif quser == "drawing":
-    quser4 = input("open (Clip Studio Paint (csp)|Infinite Painter|DeviantArt|artstation|tumblr) : ").lower()
-#    if quser4 == "Clip Studio Paint (csp)" or quser4 == "csp" or quser4 == "Clip Studio Paint":
-#    elif quser4 == "Infinite Painter":
-    if quser4 == "DeviantArt":
+    quser4 = input("open (clip studio paint (csp)|infinite painter|deviantArt|artstation|tumblr) : ").lower()
+#    if quser4 == "clip studio paint (csp)" or quser4 == "csp" or quser4 == "clip studio paint":
+#    elif quser4 == "infinite painter":
+    if quser4 == "deviantArt":
       webbrowser.open("https://DeviantArt.com/") 
-      print("Opening DeviantArt...")
+      print("Opening deviantArt...")
     elif quser4 == "artstation":
       webbrowser.open("https://artstation.com/") 
       print("Opening artstation...")
@@ -110,12 +110,12 @@ while True:
     else:
       print("This app & web-site does not exist")
   elif quser == "internet":
-    queser16 = input(" open (Social media|Browser|google) : ").lower()
-    if queser16 == "Social media":
-      queser17 = input(" open (YouTube|tumblr|x (twitter)|instagram|pinterest|behance|whatsapp|telegram|dribbble) : ").lower()
-      if queser17 == "YouTube":
-        webbrowser.open("https://YouTube.com/") 
-        print("Opening YouTube...")
+    queser16 = input(" open (social media|browser|google) : ").lower()
+    if queser16 == "social media":
+      queser17 = input(" open (youTube|tumblr|x (twitter)|instagram|pinterest|behance|whatsapp|telegram|dribbble) : ").lower()
+      if queser17 == "youTube":
+        webbrowser.open("https://youTube.com/") 
+        print("Opening youTube...")
       elif queser17 == "tumblr":
         webbrowser.open("https://tumblr.com/") 
         print("Opening tumblr...")
@@ -133,7 +133,7 @@ while True:
         print("Opening dribbble...")
       else:
         print("This app & web-site does not exist")
-#    elif queser16 == "Browser":
+#    elif queser16 == "browser":
 #      queser18 = input("open (google chrome|firefox|Brave) : ").lower()
  #      if queser18 == "google chrome":
 #      elif queser18 == "firefox":
@@ -141,66 +141,66 @@ while True:
 #      else:
 #        print("This app & web-site does not exist")
     elif queser16 == "google":
-      queser19 = input("open (google chrome|google Search|gmail|google Drive|google Docs|google Sheets|google Slides|google Keep|google Maps|google Earth|google Fonts|google translate|google Play Store|google Account) : ").lower()
+      queser19 = input("open (google chrome|google search|gmail|google drive|google docs|google Sheets|google slides|google keep|google maps|google earth|google fonts|google translate|google play store|google account|calendar) : ").lower()
 #      if queser19 == "google chrome":
-      if queser19 == "google Search":
+      if queser19 == "google search":
         webbrowser.open("https://www.google.com/") 
         print("Opening google...")
       elif queser19 == "gmail":
         webbrowser.open("https://mail.google.com/mail/u/0/#inbox") 
         print("Opening mail...")
-      elif queser19 == "google Drive":
+      elif queser19 == "google drive":
         webbrowser.open("https://drive.google.com/drive/home") 
         print("Opening drive...")
-      elif queser19 == "google Docs":
+      elif queser19 == "google docs":
         webbrowser.open("https://docs.google.com/document/u/0/") 
         print("Opening docs...")
       elif queser19 == "google Sheets":
         webbrowser.open("https://docs.google.com/spreadsheets/u/0/") 
         print("Opening Sheets...")
-      elif queser19 == "google Slides":
+      elif queser19 == "google slides":
         webbrowser.open("https://docs.google.com/presentation/u/0/") 
-        print("Opening Slides...")
-      elif queser19 == "google Calendar":
+        print("Opening slides...")
+      elif queser19 == "google calendar":
         webbrowser.open("https://calendar.google.com/calendar/u/0/r?pli=1") 
-        print("Opening Calendar...")
-      elif queser19 == "google Keep":
+        print("Opening calendar...")
+      elif queser19 == "google keep":
         webbrowser.open("https://keep.google.com/") 
-        print("Opening Keep...")
-      elif queser19 == "google Maps":
+        print("Opening keep...")
+      elif queser19 == "google maps":
         webbrowser.open("https://www.google.com/maps") 
-        print("Opening Maps...")
-      elif queser19 == "google Earth":
+        print("Opening maps...")
+      elif queser19 == "google earth":
         webbrowser.open("https://earth.google.com/web/") 
-        print("Opening google Earth...")
-      elif queser19 == "google Play Store":
+        print("Opening google earth...")
+      elif queser19 == "google play store":
         webbrowser.open("https://play.google.com/store/games?hl=en") 
-        print("Opening Play Store...")
+        print("Opening play store...")
 #      elif queser19 == "google Photos":
-      elif queser19 == "google AdSense":
+      elif queser19 == "google adsense":
         webbrowser.open("https://adsense.google.com/start/") 
-        print("Opening AdSense...")
+        print("Opening adsense...")
       elif queser19 == "google translate":
         webbrowser.open("https://translate.google.com/?sl=ar&tl=en&op=translate") 
         print("Opening translate...")
-      elif queser19 == "google Account":
+      elif queser19 == "google account":
         webbrowser.open("https://myaccount.google.com/?utm_source=OGB&utm_medium=app") 
-        print("Opening Account...")
-      elif queser19 == "google Fonts":
+        print("Opening account...")
+      elif queser19 == "google fonts":
         webbrowser.open("https://fonts.google.com/") 
-        print("Opening Fonts...")
+        print("Opening fonts...")
       else:
         print("This app & web-site does not exist")
     else:
       print("This app & web-site does not exist")
-  elif quser == "Freelance":
-    queser15 = input("open (behance|Freelancer|dribbble|kafiil|khamsat|mostaql) : ").lower()
+  elif quser == "freelance":
+    queser15 = input("open (behance|freelancer|dribbble|kafiil|khamsat|mostaql) : ").lower()
     if queser15 == "behance":
       webbrowser.open("https://behance.com/") 
       print("Opening behance...")
-    elif queser15 == "Freelancer":
-      webbrowser.open("https://Freelancer.com/") 
-      print("Opening Freelancer...")
+    elif queser15 == "freelancer":
+      webbrowser.open("https://freelancer.com/") 
+      print("Opening freelancer...")
     elif queser15 == "dribbble":
       webbrowser.open("https://dribbble.com/") 
       print("Opening dribbble...")
@@ -216,26 +216,26 @@ while True:
     else:
       print("This app & web-site does not exist")
   elif quser == "games":
-    queser14 = input("open (steam|Asphalt Legends - Racing Game|Genshin Impact|eFootball|Dummynation|Forza Horizon 5|territorial io) : ").lower()
+    queser14 = input("open (steam|asphalt legends - racing game|genshin impact|EA SPORTS FC™ Mobile Soccer|Dummynation|Forza Horizon 5|territorial io) : ").lower()
     if queser14 == "steam":
       webbrowser.open("https://store.steampowered.com/") 
       print("Opening steam...")
-#    elif queser14 == "Asphalt Legends - Racing Game":
+#    elif queser14 == "asphalt legends - racing game":
     elif queser14 == "territorial io":
       webbrowser.open("https://territorial.io/")
       print("Opening territorial io...")
-#    elif queser14 == "Genshin Impact":
+#    elif queser14 == "genshin impact":
 #    elif queser14 == "eFootball":
 #    elif queser14 == "Dummynation":
 #    elif queser14 == "Forza Horizon 5":
 #    else:
 #      print("This app & web-site does not exist")
   elif quser == "chine":
-    quser11 = input("open (rednote|bilibili|baidu|manga|anime|Weibo) : ").lower()
+    quser11 = input("open (rednote|bilibili|baidu|manga|anime|weibo) : ").lower()
 #    if quser11 == "rednote":
-    if quser11 == "Weibo":
+    if quser11 == "weibo":
       webbrowser.open("https://m.weibo.cn/") 
-      print("Opening Weibo...")
+      print("Opening weibo...")
     elif quser11 == "bilibili":
       webbrowser.open("https://www.bilibili.com/") 
       print("Opening bilibili...")
@@ -243,9 +243,9 @@ while True:
       webbrowser.open("https://www.baidu.com/") 
       print("Opening baidu...")
     elif quser11 == "manga":
-      quser12 = input("open (مانجا للشباب|مانجا للصغار|webtoons) : ").lower()
-#      if quser12 == "مانجا للشباب":
-#      elif quser12 == "مانجا للصغار":
+      quser12 = input("open (1 = مانجا للشباب = 2|مانجا للصغار|webtoons) : ").lower()
+#      if quser12 == "1":
+#      elif quser12 == "2":
       if quser12 == "webtoons":
         webbrowser.open("https://webtoons.com/") 
         print("Opening webtoons...")
